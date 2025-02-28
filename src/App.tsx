2522de888c3d83279fc30dfb8a2712ac53adc846
rohinirao5724 <22bk1a1283@stpetershyd.com>
2025-02-28 11:27:39 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import AppLogin from './components/AppLogin'
+import Registration from './components/Registration'
 
 export default function App(){
 
@@ -10,6 +11,7 @@ export default function App(){
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<AppLogin/>} />
+        <Route path="/reg" element={<Registration/>} />
       </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+
+
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="/add">landing page</a>
+        <a className="nav-link" href="/">plant categories</a>
+        <a className="nav-link" href="/search">plant details page</a>
+        <a className="nav-link" href="/delete">user registration/login</a>
+        <a className="nav-link" href="/delete">user shopping cart</a>
+        <a className="nav-link" href="/delete">user search & Filters</a>
+        <a className="nav-link" href="/delete">user order Histort</a>
+       
+       </div>
+    </div>
+  </div>
+</nav>
+
+
+
+    </div>
+  )
+}
